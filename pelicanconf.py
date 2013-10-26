5226a5f027dbf8e85116ebd588d086d53bf5e788
Greg Reda <gjreda+github@gmail.com>
2013-10-26 13:57:56 -0500

    Change things a bit to work with the new notebook theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,31 +5,44 @@ AUTHOR = u'Greg Reda'
 SITENAME = u'Greg Reda'
 SITEURL = 'http://www.gregreda.com'
 TIMEZONE = 'America/Chicago'
-THEME = './theme/simply'
+THEME = './theme/notebook-simpler'
 SUMMARY_MAX_LENGTH = 50
-# AVATAR = './theme/images/avatar.jpg'
-TITLE = "Greg Reda: Data geek in Chicago."
+AVATAR = '/theme/images/avatar.jpg'
+TITLE = "Greg Reda: Data nerd in Chicago."
 DESCRIPTION = "Greg Reda is a Chicagoan focused on analyzing data to provide insight and drive decisions. He also loves stats, visualization, beer, and music."
 
 ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}'
 ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
 
+
 # DEFAULTS
 DEFAULT_LANG = 'en'
 DEFAULT_CATEGORY = 'misc'
 DEFAULT_DATE = 'fs'
-DEFAULT_DATE_FORMAT = '%B %d, %Y'
+DEFAULT_DATE_FORMAT = '%d %b %Y'
 DEFAULT_PAGINATION = False
 
-DISQUS_SITENAME = "gregreda"
-GOOGLE_ANALYTICS = "UA-34295039-1"
-DOMAIN = "gregreda.com"
 
 # FEEDS
-FEED_ATOM = "feeds/all.atom.xml"
+FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = "feeds/%s.atom.xml"
 TAG_FEED_ATOM = "feeds/tag/%s.atom.xml"
 
-# STATIC_PATHS = ["images"]
-# FILES_TO_COPY = (("extra/robots.txt", "robots.txt"), )
 
+# PLUGINS
+PLUGIN_PATH = '../pelican-plugins'
+PLUGINS = ['liquid_tags.notebook', 'liquid_tags.include_code',
+            'liquid_tags.img', 'liquid_tags.video']
+
+CODE_DIR = 'code'
+NOTEBOOK_DIR = 'notebooks'
+EXTRA_HEADER = open('_nb_header.html').read().decode('utf-8')
+
+STATIC_PATHS = ['images', 'code', 'notebooks', 'extra']
+EXTRA_PATH_METADATA = {'extra/robots.txt': {'path': 'robots.txt'},}
+
+
+# Additional
+DISQUS_SITENAME = "gregreda"
+GOOGLE_ANALYTICS = "UA-34295039-1"
+DOMAIN = "gregreda.com"
